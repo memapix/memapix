e56a921f080cfbb70b14eb0ff9e38f70a0ade1b8
Alice Pao <gusjaq@gmail.com>
2016-07-21 18:40:32 -0700

    Add more test seed data for Photo model, create test seed data for Tag model, update string rep for models, and create load script for Tag model

--- memapixapp/load_data_txt.py
+++ memapixapp/load_data_txt.py
@@ -5,7 +5,7 @@ import os
 from django.utils import timezone
 
 
-def load_data_txt_to_models():
+def load_data_txt_to_photo_model():
     """Read data file and create Photo model objects."""
 
     filepath = os.path.abspath('memapixapp/static/memapixapp/data/photo_test.txt')
@@ -17,3 +17,19 @@ def load_data_txt_to_models():
             new_photo.save()  # must save for data to be in database
 
     return "All photos from data file created, added to database and saved."
+
+
+def load_data_txt_to_tag_model():
+    """Read data file and create Tag model objects."""
+
+    filepath = os.path.abspath('memapixapp/static/memapixapp/data/tag_test.txt')
+
+    with open(filepath) as f:
+        for line in f:
+            tag_text_from_file = line.strip()
+            new_tag = Tag(tag_text=tag_text_from_file)
+            new_tag.save()
+
+    return "All tags from data file created, added to database and saved."
+
+
--- memapixapp/tests.py
+++ memapixapp/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
+from memapixapp.load_data_txt import load_data_txt_to_photo_model, load_data_txt_to_tag_model
 
-# Create your tests here.
+
