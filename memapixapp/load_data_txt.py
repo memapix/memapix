b956255d09f40798bf1577e7aaced9a758d8a717
SHALINI PYAPALI <spyapali@SHALINIs-MacBook-Pro.local>
2016-07-21 19:07:24 -0700

    Create test seed data and load scripts for User and Album models

--- memapixapp/load_data_txt.py
+++ memapixapp/load_data_txt.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 
 from memapixapp.models import Photo, Tag, Album, User
 
-def load_data_txt():
-=======
 """Loads text data into models."""
 
 from memapixapp.models import Photo, Tag, Album, User
@@ -23,4 +20,39 @@ def load_data_txt_to_models():
             new_photo.save()  # must save for data to be in database
 
     return "All photos from data file created, added to database and saved."
->>>>>>> 7f5bef2b3890160066fdc644e3fb9ff04e68a47e
+
+
+def load_data_txt_to_user_model():
+    """Read data file and create User model objects."""
+
+    filepath = os.path.abspath('memapixapp/static/memapixapp/data/user_test.txt')
+
+    with open(filepath) as f:
+        for line in f:
+            username, password, first_name, last_name, email, date = line.strip().split("|") # split the line by the delimeter "|" and unpack User attributes.
+            new_user = User(username=username, password=password, first_name=first_name, last_name=last_name, email=email, date_joined=timezone.now())
+            new_user.save()
+
+    return "All users from data file created, added to database and saved."
+
+
+
+def load_data_txt_to_album_model():
+    """Read data file and create Album model objects."""
+
+    filepath = os.path.abspath('memapixapp/static/memapixapp/data/album_test.txt')
+
+    user = User.objects.get(pk=1)
+    with open(filepath) as f:
+        for line in f:
+            title = line.strip()
+            new_album = Album(user=user, title=title, date_created=timezone.now())
+            new_album.save()
+
+
+    return "All albums from data file created, added to database and saved."
+
+
+
+
+
